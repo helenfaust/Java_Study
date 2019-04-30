054b65affe790dd1789fb3061c6323a4fab65b30
helenfaust <helenfaust@bk.ru>
2019-04-30 17:37:25 +0200

    Изменены настройки форматирования

--- sandbox/src/main/java/HF/study/sandbox/studyProgram.java
+++ sandbox/src/main/java/HF/study/sandbox/studyProgram.java
@@ -1,8 +1,9 @@
 package HF.study.sandbox;
 
-public class studyProgram {
-		public static void main(String[] args) {
-		System.out.println("What a wonderful day");
-	}
+public class StudyProgram {
+
+  public static void main(String[] args) {
+    System.out.println("What a wonderful day");
+  }
 
 }
