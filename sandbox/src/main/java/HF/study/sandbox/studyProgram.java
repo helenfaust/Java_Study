d0bd857559587a3f9449d04cfbcd15cf6a78edbe
helenfaust <helenfaust@bk.ru>
2019-05-04 10:28:38 +0200

    Добавлено несколько функций

--- sandbox/src/main/java/HF/study/sandbox/studyProgram.java
+++ sandbox/src/main/java/HF/study/sandbox/studyProgram.java
@@ -3,7 +3,27 @@ package HF.study.sandbox;
 public class StudyProgram {
 
   public static void main(String[] args) {
-    System.out.println("What a wonderful day");
+    hello("life");
+    hello("morning");
+    hello("spring");
+
+    double l = 3;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+     }
+
+  public static void hello(String somebody) {
+        System.out.println("What a wonderful " + somebody + "!");
   }
 
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
